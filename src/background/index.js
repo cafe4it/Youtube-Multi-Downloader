67cbc798e5a7a1e9d6a2308c7c61203a62625f97
cafe4it <cafe4it@gmail.com>
2016-05-23 22:58:38 +0700

    parse simple

--- src/background/index.js
+++ src/background/index.js
@@ -28,8 +28,7 @@
  tracker.sendAppView('App view');
  });
  */
-
-
+import _ from 'lodash';
 import {createStore} from 'redux';
 import rootReducer from './reducers';
 
@@ -42,8 +41,58 @@ const store = createStore(rootReducer, {}); // a normal Redux store
 wrapStore(store, {portName: 'MY_APP'});
 
 chrome.webRequest.onCompleted.addListener((details)=> {
-	if(details.type === 'main_frame' || details.type === 'sub_frame'){
-		console.log('current : ', details.url);
-		store.dispatch(addCurrentVideo(details.url));
-	}
-}, {urls: ["*://*.youtube.com/watch?v=*","*://*.youtube.com/embed/*"]})
+    const TYPES = ['main_frame', 'sub_frame', 'xmlhttprequest'];
+    if (_.some(TYPES, (t)=> t === details.type)) {
+        function extractVideoID(url){
+            var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+            var match = url.match(regExp);
+            if ( match && match[7].length == 11 ){
+                return match[7];
+            }else{
+                return null;
+            }
+        }
+        const getInfoUrl = 'https://www.youtube.com/get_video_info?video_id=' + extractVideoID(details.url);
+        let myWorker = new Worker(chrome.runtime.getURL('shared/worker.js'));
+        myWorker.onmessage = function (e) {
+            let formats = parseYoutubeInfoStringToFormats(e.data);
+            console.info(formats);
+        }
+        myWorker.postMessage({url : getInfoUrl});
+
+        store.dispatch(addCurrentVideo(details.url));
+    }
+}, {urls: ["*://*.youtube.com/watch?v=*", "*://*.youtube.com/embed/*"]})
+
+let parseYoutubeInfoStringToFormats = function(youtubeInfoString) {
+    var element, formatInfoArray, formatInfoElement, formatInfoElementPair, formatInfoMap, formatStreamArray, formatStreamArrayString, formatStreamString, formats, i, j, len, len1, youtubeInfoArray;
+    youtubeInfoArray = youtubeInfoString.split('&');
+    if (youtubeInfoArray[0] === 'status=fail') {
+        return null;
+    }
+    formatStreamArrayString = ((function() {
+        var i, len, results;
+        results = [];
+        for (i = 0, len = youtubeInfoArray.length; i < len; i++) {
+            element = youtubeInfoArray[i];
+            if (element.split('=')[0] === 'url_encoded_fmt_stream_map') {
+                results.push(element);
+            }
+        }
+        return results;
+    })())[0].split('=')[1];
+    formatStreamArray = decodeURIComponent(formatStreamArrayString).split(',');
+    formats = [];
+    for (i = 0, len = formatStreamArray.length; i < len; i++) {
+        formatStreamString = formatStreamArray[i];
+        formatInfoArray = formatStreamString.split('&');
+        formatInfoMap = {};
+        for (j = 0, len1 = formatInfoArray.length; j < len1; j++) {
+            formatInfoElement = formatInfoArray[j];
+            formatInfoElementPair = formatInfoElement.split('=');
+            formatInfoMap[formatInfoElementPair[0]] = decodeURIComponent(formatInfoElementPair[1]);
+        }
+        formats.push(formatInfoMap);
+    }
+    return formats;
+};
--- src/shared/worker.js
+++ src/shared/worker.js
@@ -1,3 +1,10 @@
-onmessage = function(e){
-
+onmessage = function (e) {
+    var url = e.data.url;
+    fetch(url, {method: 'get'}).then(function (response) {
+        if (response.status === 200) {
+            return response.text()
+        }
+    }).then(function (text) {
+        if (text)postMessage(text);
+    })
 }
